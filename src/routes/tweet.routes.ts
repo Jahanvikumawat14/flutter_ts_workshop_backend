6012a81d1c6643367dde3d3ad03854c465c74acc
Jahanvikumawat14 <kumawatjahanvi14@gmail.com>
2024-05-10 18:01:03 +0530

    init

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -7,7 +7,7 @@ const tweetRouter = Router();
 // Define route paths
 
 tweetRouter.get("/:tweetId", getTweetController)
-//tweetRouter.get("/get/all", getAllTweetsController)
+tweetRouter.get("/get/all", getAllTweetsController)
 tweetRouter.post("/", createTweetController)
 tweetRouter.delete("/:tweetId", deleteTweetController)
 tweetRouter.put("/", updateTweetController)
